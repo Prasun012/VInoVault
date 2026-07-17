46a9f536c618c83f85a7d1648e982073f364804c
Prasun <prasunshrestha51@gmail.com>
2026-07-17 21:32:21 +0545

    Calling APi Tesrt

--- src/core/Home.jsx
+++ src/core/Home.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react"
+import { searchDrink } from "../api/Drinks"
+
 export function Home()
 {
-
+    useEffect(() => {
+        async function loadData(){
+            const drinks= await searchDrink("margarita")
+            console.log(drinks);
+            
+        };
+        loadData();
+    },[])
 
     return(
         <h1>Home</h1>
