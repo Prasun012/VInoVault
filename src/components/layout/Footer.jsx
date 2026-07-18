2fbf35f0364950b0532fdea3a28748572d2501a3
Prasun <prasunshrestha51@gmail.com>
2026-07-18 09:07:30 +0545

    day2 start

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ export function Footer(){
     
 
     return(
-        <div className="w-full flex item-center justify-between px-6 boarder-t bparder-slate-300 bg-gray-200">
+        <div className="w-full flex item-center justify-center px-6 boarder-t bparder-slate-300 bg-gray-200">
             @VIniVault -Made by Prasun Shrestha
         </div>
     )
